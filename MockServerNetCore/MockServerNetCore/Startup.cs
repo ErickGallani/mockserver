53b6dfd7897d6277fda4fc8bdbf0cfd72a9de15f
Erick Gallani <erick.gallani.1@gmail.com>
2018-02-01 00:24:55 +0000

    Start the test project

---
+++ MockServerNetCore/MockServerNetCore/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WireMock.Server;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+namespace MockServerNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var startMockServer = Configuration.GetValue<bool>("StartMockServer");
+
+            if (startMockServer)
+            {
+                var server = FluentMockServer.Start(port: 8443);
+
+                server
+                    .Given(Request.Create().WithUrl(u => u.Contains("teste")).UsingGet())
+                    .RespondWith(Response.Create()
+                        .WithStatusCode(200)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(@"{ ""msg"": ""Hello world!""}")
+                    );
+            }
+
+            app.UseMvc();
+        }
+    }
+}
